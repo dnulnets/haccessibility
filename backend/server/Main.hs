b3362dd7cefdee81bf609a647d9766b10ed8ce6d
Tomas Stenlund <tomas.stenlund@telia.com>
2020-01-26 22:18:30 +0100

    Added missing directory

---
+++ backend/server/Main.hs
@@ -0,0 +1,19 @@
+-- |
+-- Module      : Main
+-- Description : The main entry point for the accessability grapql server
+-- Copyright   : (c) Tomas Stenlund, 2019
+-- License     : BSD-3
+-- Maintainer  : tomas.stenlund@permobil.com
+-- Stability   : experimental
+-- Portability : POSIX
+-- 
+-- This module contains the initialization and setup of the graphql API for the
+-- accessability database.
+--
+
+module Main where
+
+import Boot
+    
+main :: IO ()
+main = serverMain
